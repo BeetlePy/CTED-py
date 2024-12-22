fec79d5f892a06da5e3d8487d2a7bd3ed31922a2
BeetlePy <188133089+BeetlePy@users.noreply.github.com>
2024-12-21 18:50:46 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import better_exceptions
+better_exceptions.hook()
+def error1(input):
+    return input + 1
+
+def error2(input):
+    return input + 2
+
+def str_erro(input):
+    return str(input)
+
+def real_error(input):
+    return input + 1
+
+input = 1
+a = error1(input)
+b = error2(a)
+c = str_erro(b)
+d = real_error(c)
+print(d)

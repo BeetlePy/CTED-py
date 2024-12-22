a22f4cb13a98c0b2b1950b0e41894cbe029c6d73
BeetlePy <188133089+BeetlePy@users.noreply.github.com>
2024-12-21 23:29:18 -0600

    .

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import traceback
+from inspect import signature
+import inspect
 
 class CTEDExceptions:
 
@@ -9,17 +11,25 @@ class CTEDExceptions:
         sys.excepthook = self.custom_exception
 
     def custom_exception(self, type, value, _traceback):
+        for frame_info in inspect.trace():
+            print(f"Function: {frame_info.function}")
+            print(f"Line: {frame_info.lineno}")
+            print(f"Code: {frame_info.code_context}")
+            print(f"File: {frame_info.filename}")
+            print("-" * 30)
         tb = _traceback
         traceback_message = "This is a test. An error occured."
         print(traceback_message)
         formatted_tb = traceback.format_exception(type, value, tb) 
-        tb = self.replaceWithRelativePath(formatted_tb) 
+        tb = self.createCustomTrace(formatted_tb) 
         print("\n".join(tb))  # Print the formatted traceback
 
-    def replaceWithRelativePath(self, exception):
+    def createCustomTrace(self, exception):
         new_exception = []
         for line in exception:
             if "File " in line:
+                #unction_name = line.split('in ')[-1].split('(')[0]
+                #print(f'module:  {list(function_name)}')
                 path_start_index = line.find('"') + 1 
                 path_end_index = line.find('"', path_start_index) 
                 if path_end_index == -1: 
@@ -32,3 +42,4 @@ class CTEDExceptions:
                 new_exception.append(rel_path_tb)
             else: new_exception.append(line)
         return new_exception
+                    
